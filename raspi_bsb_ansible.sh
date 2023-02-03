883d361f49adf452cb26efd736623be874a7f6b7
naveenandrew19 <111980682+naveenandrew19@users.noreply.github.com>
2023-02-03 17:51:32 +0530

    Update raspi_bsb_ansible.sh

--- raspi_bsb_ansible.sh
+++ raspi_bsb_ansible.sh
@@ -26,9 +26,9 @@ sudo apt install docker.io -y
 
 #docker compose installation
 sudo apt-get update
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-$(uname -s)-$(uname -m)"  -o /usr/local/bin/docker-compose
+sudo mv /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
 sudo service docker restart
 sudo docker compose version
 
